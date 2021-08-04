4fe1b673420bc335314e7102d6fdf3d657374a2a
Joãõ Paulo Moretti <joaopaulomrtt@gmail.com>
2021-08-04 03:36:24 -0300

    mock-de-design

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Header } from '../Header/Header'
+
+export class Cart extends React.Component {
+    render() {
+        return <div>
+                oi
+        </div>
+    }
+}
---
+++ src/components/Register/RegisterStyles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+
+export const Menu = styled.div`
+padding-top: 200px;
+border: 1px solid black;
+display: flex;
+flex-direction: column;
+width: 50vw;
+margin: auto;
+`
+
+export const H1 = styled.h1`
+    text-align:center;
+`
+
+export const Input = styled.input`
+    width: 50%;
+    margin: auto;
+    margin-bottom: 10px;
+    padding: 15px;
+`
+
+export const Select = styled.select`
+    width: 50%;
+    margin: auto;
+    margin-bottom: 10px;
+    padding: 15px;
+`
+
+export const Label = styled.label`
+    text-align: center;
+`
